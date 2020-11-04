8d0a6a4f1199078eac28285557b69e25fdce7c91
Henry Lee <henryleeworld@gmail.com>
2020-11-04 20:55:29 +0800

    feat:升級至 laravel 8

--- app/Charts/UserLineChart.php
+++ app/Charts/UserLineChart.php
@@ -4,13 +4,41 @@
 
 namespace App\Charts;
 
+use App\Models\User;
 use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
+use DB;
 use Illuminate\Http\Request;
-use App\User;
 
 class UserLineChart extends BaseChart
 {
+    /**
+     * Determines the chart name to be used on the
+     * route. If null, the name will be a snake_case
+     * version of the class name.
+     */
+    public ?string $name = 'user_line_chart';
+
+    /**
+     * Determines the name suffix of the chart route.
+     * This will also be used to get the chart URL
+     * from the blade directrive. If null, the chart
+     * name will be used.
+     */
+    public ?string $routeName = 'user_line_chart';
+
+    /**
+     * Determines the prefix that will be used by the chart
+     * endpoint.
+     */
+    public ?string $prefix = '';
+
+    /**
+     * Determines the middlewares that will be applied
+     * to the chart endpoint.
+     */
+    public ?array $middlewares = [];
+
     /**
      * Handles the HTTP request for the given chart.
      * It must always return an instance of Chartisan
@@ -18,9 +46,9 @@ class UserLineChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
-        $users = User::select(\DB::raw("COUNT(*) as count"))
+        $users = User::select(DB::raw("COUNT(*) as count"))
                  ->whereYear('created_at', '2020')
-                 ->groupBy(\DB::raw("Month(created_at)"))
+                 ->groupBy(DB::raw("Month(created_at)"))
                  ->pluck('count');
         return Chartisan::build()
                ->labels(['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'])
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('chart-line', 'ChartController@chartLine');
+Route::get('chart-line', [ChartController::class, 'chartLine']);
